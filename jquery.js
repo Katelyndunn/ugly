06d1fbf386f0798bd653d8c5916328f94e023f73
KatelynDunn <katelyndunn@cox.net>
2015-07-17 21:09:24 +0000

    FINAL

--- jquery.js
+++ jquery.js
@@ -1,7 +1,14 @@
 $(function(){
- 
-  $(".ugly-title").click(function(){
+
+$(".header").click(function(){
+  $(this).text("Isn't this better?");
+});
+
+$(".ugly-title").click(function(){
 		$(this).removeClass("ugly-title");
+    $(this).removeClass("animated");
+    $(this).removeClass("infinite");
+    $(this).removeClass("flash");
     $(this).addClass("pretty-title");
 	});
 $(".ugly-text").click(function(){
@@ -12,20 +19,23 @@ $(".ugly-text").click(function(){
   
 $(".ugly-greeter").click(function(){
   $(this).removeClass("ugly-greeter");
+    $(this).removeClass("animate");
+    $(this).removeClass("slideInRight");
+    $(this).removeClass("slideOutLeft");
     $(this).addClass("pretty-greeter");
     $(this).addClass("col-xs-8 col-xs-offset-2");
+    $(this).text("Isn't this better?");
 });
   
 $(".ugly-image").click(function(){
   $(this).removeClass("ugly-image");
     $(this).addClass("pretty-image");
     $(this).addClass("col-xs-8 col-xs-offset-2");
+    $(".congrats").attr("Congrats! You've Fixed It!")
 });
 
-$(".ugly-body").click(function(){
-  $(this).removeClass("ugly-body");
-    $(this).addClass("pretty-body");
-});
+
+
 $("#b1").click(function(){
   $("body").removeClass("ugly-body");
     $("body").addClass("pretty-body");
@@ -34,7 +44,7 @@ $("#b1").click(function(){
   
   $(".ugly-image").click(function(){
   $(this).removeClass("ugly-image");
-    $(this).addClass("pretty-body");
+    $(this).addClass("pretty-image");
     $(this).attr("src","http://www.pitt.edu/~btb25/happycat.jpg");
 });
 
