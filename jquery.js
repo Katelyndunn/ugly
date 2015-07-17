8e67ebeeaafde40c4e9d4516eb28f2e295dd828f
KatelynDunn <katelyndunn@cox.net>
2015-07-17 17:44:20 +0000

    new classes

---
+++ jquery.js
@@ -0,0 +1,17 @@
+$(function(){
+ 
+  $(".ugly-title").click(function(){
+		$(this).removeClass("ugly-title");
+    $(this).addClass("pretty-title");
+	});
+$(".ugly-p1").click(function(){
+		$(this).removeClass("ugly-p1");
+    $(this).addClass("pretty-p1");
+	});
+
+
+
+
+
+
+});
